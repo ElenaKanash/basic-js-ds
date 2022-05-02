ad786ce14f2c582fd0aebc57ac2775b930b9e67c
Elena Kanash <hird250@gmail.com>
2022-05-02 18:55:57 +0300

    feat: complete task

--- src/queue.js
+++ src/queue.js
@@ -13,21 +13,49 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+
+class Node {
+  constructor(value) {
+      this.value = value;
+      this.next = null;
+  }
+}
 class Queue {
+  constructor() {
+    this.head = null;  // the link to the first node in Queue
+    this.tail = null;  //the link to the last node in Queue
+    this.length = 0;
+}
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
+   // throw new NotImplementedError('Not implemented'); // returns { value: 3, next: null }
     // remove line with error and write your code here
+    return this.head;
+
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  enqueue( value ) {
+   // throw new NotImplementedError('Not implemented');  //adds the element to the end of queue
     // remove line with error and write your code here
+    const node = new Node(value);
+
+    if(this.head) {
+      this.tail.next = node;
+      this.tail = node;
+    } else {
+      this.head = node;
+      this.tail = node;
+    }
+    this.length++;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
+    //throw new NotImplementedError('Not implemented'); //returns the top element from queue and deletes it
     // remove line with error and write your code here
+    let cur = this.head;
+    this.head = this.head.next;
+    this.length--;
+    return cur.value;
   }
 }
 
